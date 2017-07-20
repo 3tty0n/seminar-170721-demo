54aac4271118076ac4fb804f3cc33bd325af4a6a
Yusuke Izawa <yuizalp@gmail.com>
2017-07-21 01:22:10 +0900

    add fib.py

---
+++ fib.py
@@ -0,0 +1,19 @@
+import time
+
+def fib(n):
+    if n <= 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def main():
+    start = time.time()
+    for i in xrange(1, 30):
+        print fib(i)
+
+    elapsed_time = time.time() - start
+    print("elapsed_time:{0}".format(elapsed_time))
+
+if __name__ == "__main__":
+    main()
